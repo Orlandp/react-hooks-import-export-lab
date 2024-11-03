b24946713f022c8e71ca9c74d6fe334a4472886b
pknowledge <jamesblssings@gmail.com>
2024-11-03 10:10:46 +0300

    import export lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from './NavBar'; // Adjust the path as necessary
+import Home from './Home'; // Adjust the path as necessary
+import About from './About'; // Adjust the path as necessary
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+import { username } from '../data/user'; // Adjust the path as necessary
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>Home</h1>
+      <p>Username: {username}</p> {/* Using the username variable */}
     </div>
   );
 }
+
+export default Home;
